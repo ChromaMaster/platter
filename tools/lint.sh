bbaa592cf0610608a850f4dafbb9564bc95bedd3
ChromaMaster <vladtel.dev@gmail.com>
2024-02-29 20:18:23 +0100

    ci: optimize gonangci-lint by passing local cache
    Signed-off-by: Vlad Alexandru Teletin <vladtel.dev@gmail.com>

--- tools/lint.sh
+++ tools/lint.sh
@@ -8,4 +8,11 @@ IMAGE_VERSION="v1.56.2"
 LINTER_ARGS="--fix"
 
 # https://golangci-lint.run/usage/install/#docker
-podman run --rm -v $(pwd):/app -w /app $IMAGE:$IMAGE_VERSION golangci-lint run $LINTER_ARGS $@
+podman run --rm \
+	-v $(pwd):/app \
+	-v $(go env GOCACHE):/cache/go \
+	-v $(go env GOPATH)/pkg:/go/pkg:ro \
+	-e GOCACHE=/cache/go \
+	-e GOLANGCI_LINT_CACHE=/cache/go \
+	-w /app \
+	$IMAGE:$IMAGE_VERSION golangci-lint run --verbose $LINTER_ARGS $@
